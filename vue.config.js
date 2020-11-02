e6afd0754f46eb101e2ba06b388b938c5c4f450b
Carol <abc61958761@gmail.com>
2020-11-02 16:10:22 +0800

    fix vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,14 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/resume/'
-    : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/resume/'
+  //   : '/',
+  configureWebpack: {
+    performance: {
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+    }
+  }
+  
 }
